0e3368f2fe3ad749677f8752a03681d5cf0b33a1
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2021-05-22 12:32:36 +0200

    GoldenLayout läuft

---
+++ src/test.ts
@@ -0,0 +1,70 @@
+import { GoldenLayout, LayoutConfig } from "golden-layout";
+
+namespace Test {
+  window.addEventListener("load", () => {
+    let glMain = new GoldenLayout();
+
+    let layoutConfigForGoldenLayout: LayoutConfig = {
+      root: {
+        type: "row",
+        content: [
+          {
+            type: "component",
+            componentType: "test"
+          },
+
+          {
+            type: "component",
+            componentType: "test 2"
+          },
+
+          {
+            type: "component",
+            componentType: "test 3"
+          },
+
+          {
+            type: "component",
+            componentType: "test 4"
+          },
+
+        ]
+      }
+    }
+
+    glMain.registerComponentFactoryFunction("test", (container, itemConfig) => {
+      //console.log(container)
+      let el = document.createElement("h1");
+      el.textContent = "This is a test";
+      container.element.append(el);
+      console.log("add entry");
+    });
+
+    glMain.registerComponentFactoryFunction("test 2", (container, itemConfig) => {
+      //console.log(container)
+      let el = document.createElement("h1");
+      el.textContent = "This is a test 2";
+      container.element.append(el);
+      console.log("add entry");
+    });
+
+    glMain.registerComponentFactoryFunction("test 3", (container, itemConfig) => {
+      //console.log(container)
+      let el = document.createElement("h1");
+      el.textContent = "This is a test 3";
+      container.element.append(el);
+      console.log("add entry");
+    });
+
+    glMain.registerComponentFactoryFunction("test 4", (container, itemConfig) => {
+      //console.log(container)
+      let el = document.createElement("h1");
+      el.textContent = "This is a test 4  JIRKA";
+      container.element.append(el);
+      console.log("add entry");
+    });
+
+    glMain.loadLayout(layoutConfigForGoldenLayout);
+    console.log(glMain)
+  })
+}
