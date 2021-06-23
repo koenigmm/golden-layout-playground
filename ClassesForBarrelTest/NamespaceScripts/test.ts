dfa9f5c31f3d70fe4a64ad64f8ecc8167801e51a
Marius König <marius.koenig@outlook.com>
2021-06-23 16:01:58 +0200

    update test.ts

--- ClassesForBarrelTest/NamespaceScripts/test.ts
+++ ClassesForBarrelTest/NamespaceScripts/test.ts
@@ -2,6 +2,7 @@ namespace TestNamespace {
     import person = Person;
     import tester = Tester;
     import color = COLOR;
+
     let p1 = new Person(12, "Tester");
     let t1 = new Tester("Tester", 12, 1);
     p1.printPerson();
