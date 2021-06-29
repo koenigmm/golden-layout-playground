5702a764a6ae426e0e38934e31115cb4fae9bcb7
Marius König <marius.koenig@outlook.com>
2021-06-29 09:03:00 +0200

    update import statement

--- gl_umd_test.ts
+++ gl_umd_test.ts
@@ -3,7 +3,7 @@
 /// <reference path="./ClassesForBarrelTest/ts/tester.ts" />
 
 // Bei Bedarf unteren Kommentar entferenen
-import type { GoldenLayout, LayoutConfig } from "./goldenLayoutBundle/types";
+import { GoldenLayout, LayoutConfig } from "./goldenLayoutBundle/types";
 //import { GoldenLayout, LayoutConfig } from "./goldenLayoutBundle/types";
 
 namespace TestWithImport {
